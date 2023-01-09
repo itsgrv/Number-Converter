ca64366e882259f97c55d0cfda97d3b78a2a0e58
itsgrv <agrawalgrv@rediffmail.com>
2023-01-09 12:57:30 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+<h1>Number Converter project</h1>
+<hr>
+
+![App Screenshot](https://raw.githubusercontent.com/ritwickrajmakhal/number-converter/master/designs/design%20%5BDesktop%5D.png)
+
+## Hello developer!
+Simple conversion of one number system to other
---
+++ script.js
@@ -0,0 +1,11 @@
+"use strict"
+// Start writing code from here
+// Declare all the global variables
+
+
+// You should define the callback function for the RESET button here.
+
+// You should define the callback function for the SWAP button here.
+
+
+// You should define the callback function for the CALCULATE button here.
